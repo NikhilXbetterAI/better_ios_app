255c25968c11ae4560e4f900a75aafbe51cee4ad
NikhilXbetterAI <nikhil@better-health.ai>
2026-05-04 10:07:38 +0530

    Initial Commit

---
+++ Better/BetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterApp.swift
+//  Better
+//
+//  Created by Nikhil Khatale on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterUITests/BetterUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BetterUITestsLaunchTests.swift
+//  BetterUITests
+//
+//  Created by Nikhil Khatale on 04/05/26.
+//
+
+import XCTest
+
+final class BetterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
